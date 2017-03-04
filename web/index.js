dca1abaa342322d5bd4904c6a63cdd1ad5ac9804
Ovidiu Dolha <odolha@gmail.com>
2017-03-04 16:52:38 +0200

    heroku change run

--- web/index.js
+++ web/index.js
@@ -1,5 +1,5 @@
 var static = require('node-static');
-var file = new static.Server();
+var file = new static.Server('./public');
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);
